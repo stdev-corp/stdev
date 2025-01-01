1c97b131e5e78529adbd893d3d8e88fe4b2473a9
Seungbin Oh <sboh1214@gmail.com>
2025-01-02 05:10:11 +0900

    chore: set trustHost to true

--- utils/auth.ts
+++ utils/auth.ts
@@ -6,4 +6,5 @@ import { prisma } from '@/utils/prisma'
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
+  trustHost: true,
 })
