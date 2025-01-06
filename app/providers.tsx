36c2f4409b8746414ec590e09a7b87986d15656a
Seungbin Oh <sboh1214@gmail.com>
2025-01-07 06:10:27 +0900

    feat: add channel talk

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,20 @@
 'use client'
+import * as ChannelService from '@channel.io/channel-web-sdk-loader'
 import { NextUIProvider } from '@nextui-org/system'
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
 
 export function Providers({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    console.log('ChannelService.loadScript()')
+    ChannelService.loadScript()
+    ChannelService.boot({
+      pluginKey: process.env.NEXT_PUBLIC_CHANNEL_PLUGIN_KEY!,
+    })
+    return () => {
+      ChannelService.shutdown()
+    }
+  }, [])
+
   return (
     <NextUIProvider style={{ minHeight: '100vh' }}>{children}</NextUIProvider>
   )
