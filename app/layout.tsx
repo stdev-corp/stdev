8c9e07cb710ab1c8b6143ba432e32e1f0bdddb4c
Seungbin Oh <sboh1214@gmail.com>
2025-01-10 01:51:40 +0900

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Providers } from './providers'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className="min-h-screen">
         <Providers>{children}</Providers>
       </body>
+      <GoogleAnalytics gaId="G-8S1XRBF33D" />
     </html>
   )
 }
