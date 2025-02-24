76728b28a491171ade6df831450625bb413ec0d0
Seungbin Oh <sboh1214@gmail.com>
2025-02-24 20:54:05 +0900

    feat: remove history

--- app/home/intro/history/page.tsx
+++ app/home/intro/history/page.tsx
@@ -1,16 +1,17 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 
 export default function HistoryPage() {
   return (
     <>
       <h1>연혁</h1>
       <div className="h-8" />
-      <Image
+      준비중
+      {/* <Image
         src="/images/intro/history-picture.png"
         alt="History of STDev Corp."
         width={800}
         height={600}
-      />
+      /> */}
     </>
   )
 }
--- app/home/intro/page.tsx
+++ app/home/intro/page.tsx
@@ -30,12 +30,12 @@ export default function IntroPage() {
       <div className="h-[120px]" />
       <Image src="/images/intro/3w1h.png" alt="3w1h" width={800} height={800} />
       <div className="h-[120px]" />
-      <Image
+      {/* <Image
         src="/images/intro/history.png"
         alt="history"
         width={800}
         height={800}
-      />
+      /> */}
       <div className="h-[120px]" />
     </div>
   )
