c291385fcd4fe76c1c730b4707ab680b472373eb
Seungbin Oh <sboh1214@gmail.com>
2025-12-10 22:59:28 +0900

    feat: add markdowns and reports 정관, 개인정보처리방침, 이용약관, 총회/이사회

--- src/app/(stdev)/notices/records/page.tsx
+++ src/app/(stdev)/notices/records/page.tsx
@@ -1,12 +1,14 @@
 import RecordList from '@/components/record-list'
 import { queryReports } from '@/utils/payload'
+import { Box, Heading } from '@chakra-ui/react'
 
 export default async function RecordsPage() {
   const reports = await queryReports('meeting')
 
   return (
     <div>
-      <h1>총회 및 이사회</h1>
+      <Heading>총회 및 이사회</Heading>
+      <Box h={4} />
       <RecordList reports={reports} />
     </div>
   )
